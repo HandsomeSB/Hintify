0172e9ec2134b7bd25f396286c2b8fa3302980a8
ad0rshcodes <adarsharma.me@gmail.com>
2025-03-01 14:22:24 -0600

    remove env from history

--- extension.js
+++ extension.js
@@ -1,22 +1,45 @@
 const vscode = require("vscode");
 const voiceRecording = require("./src/voiceRecording");
+const whisper = require("./src/wispher");
 
 function activate(context) {
-  //Registering command for startRecording
   const startRecording = vscode.commands.registerCommand(
     "hintify.startRecording",
     () => {
       const workspaceFolders = vscode.workspace.workspaceFolders;
-      voiceRecording.startRecording(workspaceFolders);
+      voiceRecording.startRecording(workspaceFolders).then((filePath) => {
+        if (filePath) {
+          console.log("test");
+          // Recording successful, send to Whisper
+          whisper
+            .transcribe(filePath)
+            .then((transcript) => {
+              if (transcript) {
+                vscode.window.showInformationMessage(
+                  "Transcription: " + transcript
+                );
+                // Optionally delete the wav file here
+                voiceRecording.deleteRecording(filePath);
+              } else {
+                vscode.window.showErrorMessage("Failed to transcribe audio.");
+                voiceRecording.deleteRecording(filePath); //delete wav file even if transciption fails.
+              }
+            })
+            .catch((err) => {
+              vscode.window.showErrorMessage(
+                "Whisper API error: " + err.message
+              );
+              voiceRecording.deleteRecording(filePath); //delete wav file if whisper api errors.
+            });
+        }
+      });
     }
   );
 
-  //Registering command for stopRecording
   const stopRecording = vscode.commands.registerCommand(
     "hintify.stopRecording",
     () => {
-      const workspaceFolders = vscode.workspace.workspaceFolders;
-      voiceRecording.stopRecording(workspaceFolders);
+      voiceRecording.stopRecording(vscode.workspace.workspaceFolders);
     }
   );
 
